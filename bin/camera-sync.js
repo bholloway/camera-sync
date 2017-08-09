1497b35d70851d7715614aef0b3fb5ddd3b3a7fc
Ben Holloway <github@bholloway.com>
2017-08-10 09:43:47 +1000

    refactor stats logging

--- bin/camera-sync.js
+++ bin/camera-sync.js
@@ -1,45 +1,32 @@
 #!/usr/bin/env node
 
-const path = require('path');
 const program = require('commander');
 
 const {version, description} = require('../package.json');
+const {multiline, simple, fileList} = require('../lib/format');
 
-const onStats = ({source, destination, images, videos, others, writes}) => {
-  const format = (list) => {
-    const text = list
-      .map(fullName => path.basename(fullName))
-      .join(', ')
-      .slice(0, 60);
-
-    return (text.length === 60) ? text.replace(/[^,]*$/, ' ...') : text;
-  };
-
-  const message = [
-    `source     : ${source}`,
-    `destination: ${destination || '-'}`,
-    images && `images     : (${images.length}) ${format(images)}`,
-    videos && `videos     : (${videos.length}) ${format(videos)}`,
-    others && `others     : (${others.length}) ${format(others)}`,
-    writes && `writes     : (${writes.length}) ${format(writes)}`
-  ]
-    .filter(Boolean)
-    .join('\n');
-
-  console.log(message);
-};
 
 const onError = (error) => {
-  console.error(error.message);
+  console.error(error);
   process.exit(1);
 };
+
+
+const logStats = formatter => stats =>
+  console.log(formatter(stats));
+
 
 program
   .command('scan <source>')
   .description('find source files')
   .action(source =>
     require('../lib/scan')({source})
-      .then(onStats)
+      .then(logStats(multiline(
+        simple('source'),
+        fileList('images'),
+        fileList('videos'),
+        fileList('others')
+      )))
       .catch(onError)
   );
 
@@ -48,7 +35,12 @@ program
   .description('test sync without writing files')
   .action((source, destination) =>
     require('../lib/test')({source, destination})
-      .then(onStats)
+      .then(logStats(multiline(
+        simple('source'),
+        simple('destination'),
+        fileList('others'),
+        fileList('updates')
+      )))
       .catch(onError)
   );
 
@@ -57,7 +49,12 @@ program
   .description('sync by writing files to the destination')
   .action((source, destination) =>
     require('../lib/sync')({source, destination})
-      .then(onStats)
+      .then(logStats(multiline(
+        simple('source'),
+        simple('destination'),
+        fileList('others'),
+        fileList('updates')
+      )))
       .catch(onError)
   );
 
